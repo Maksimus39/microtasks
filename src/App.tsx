25a3721f383614a13f8819aaa8c63a402413f8ab
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-21 23:51:55 +0300

    Микротаска по вложенным компонентам и плюсом передача данных через props и его типизация и отрисовка

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {NestedComponent} from "./Nested components/nestedComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <hr/>
+            <NestedComponent title={'Menu'}/>
+            <NestedComponent title={'User'}/>
+            <hr/>
+        </div>
+    );
 }
 
 export default App;
