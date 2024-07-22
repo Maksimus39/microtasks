9043cb501a168d0dd53389d000d8dc7ca93aa630
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-22 09:41:34 +0300

    Выполнил микротаску по методу map, отрисовал компонент и был дан исходный массив.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
 import {NestedComponent} from "./Nested components/nestedComponent";
+import {MethodMap} from "./metodMap/MetodMap";
 
 function App() {
+
+    //  map
+    const topCars = [
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'}
+    ]
+
+
     return (
         <div className="App">
             <hr/>
             <NestedComponent title={'Menu'}/>
             <NestedComponent title={'User'}/>
             <hr/>
+            <MethodMap car={topCars}/>
         </div>
     );
 }
